010225346d7ebd94901ca27d70f02153ee336a52
joysonShanks <joyson.shanks@gmail.com>
2018-11-08 23:17:00 +0530

    Adding missing changes

--- todoapp/src/main/java/com/joyson/todoapp/controller/TodoController.java
+++ todoapp/src/main/java/com/joyson/todoapp/controller/TodoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import com.joyson.todoapp.model.Todo;
 import com.joyson.todoapp.model.TodoResponse;
 import com.joyson.todoapp.service.TaskService;
 import com.joyson.todoapp.service.TodoService;
+import com.joyson.todoapp.utils.State;
 
 @RequestMapping(path = "/todo-app")
 @RestController
@@ -78,5 +80,42 @@ public class TodoController {
 	@GetMapping("/todo")
 	public ResponseEntity<List<Todo>> getAllTodo(@PathVariable Long todoId) {
 		return new ResponseEntity<List<Todo>>(todoService.findAll(), HttpStatus.OK);
+	}
+
+	@PutMapping("/completed/{taskId}/task")
+	public ResponseEntity<TodoResponse> completeTask(@PathVariable Long taskId) {
+		Optional<Task> optionalTask = taskService.findTaskById(taskId);
+		if (optionalTask.isPresent()) {
+			Task task = optionalTask.get();
+			task.setCompleted(true);
+			TodoResponse todoResponse = new TodoResponse();
+			taskService.save(task);
+			todoResponse.setId(taskId);
+			todoResponse.setMessage("Task completed successfully");
+			return new ResponseEntity<TodoResponse>(todoResponse, HttpStatus.OK);
+		} else {
+			TodoResponse todoResponse = new TodoResponse();
+			todoResponse.setId(taskId);
+			todoResponse.setMessage("Invalid task id");
+			return new ResponseEntity<TodoResponse>(todoResponse, HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PutMapping("/completed/{todoId}/todo")
+	public ResponseEntity<TodoResponse> completeTodo(@PathVariable Long todoId) {
+		Optional<Todo> optionalTodo = todoService.findById(todoId);
+		if (optionalTodo.isPresent()) {
+			Todo todo = optionalTodo.get();
+			todo.setState(State.COMPLETED);
+			TodoResponse todoResponse = new TodoResponse();
+			todoResponse.setId(todoService.save(todo));
+			todoResponse.setMessage("Todo completed successfully");
+			return new ResponseEntity<TodoResponse>(todoResponse, HttpStatus.OK);
+		} else {
+			TodoResponse todoResponse = new TodoResponse();
+			todoResponse.setId(todoId);
+			todoResponse.setMessage("Invalid todo id");
+			return new ResponseEntity<TodoResponse>(todoResponse, HttpStatus.NOT_FOUND);
+		}
 	}
 }
