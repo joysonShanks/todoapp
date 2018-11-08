54265b9cf17d312e27d57555b9dca26b2a42afe5
joysonShanks <joyson.shanks@gmail.com>
2018-11-08 23:15:44 +0530

    Adding complete task and todo api's Fixed banner.txt alignments

--- todoapp/src/main/java/com/joyson/todoapp/TodoappApplication.java
+++ todoapp/src/main/java/com/joyson/todoapp/TodoappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
 	}
